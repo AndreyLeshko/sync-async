2343d605a169892e29bab23a33d88eed589cb33b
andrey <andreyleshko2001@gmail.com>
2023-09-19 04:19:57 +0700

    NEW: add async_to_sync decorator

--- sync_to_async.py
+++ sync_to_async.py
@@ -5,4 +5,19 @@ async def sync_to_async(func):
     async def wrapper(*args, **kwargs):
         res = await asyncio.to_thread(func(*args, **kwargs))
         return res
+
+    return wrapper
+
+
+def async_to_sync(func):
+    def wrapper(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            message = ('You cannot synchronously execute an asynchronous function from a running event loop.' +
+                       'Use await syntax to run runction')
+            raise RuntimeError(message)
+        else:
+            res = loop.run_until_complete(func(*args, **kwargs))
+        return res
+
     return wrapper
